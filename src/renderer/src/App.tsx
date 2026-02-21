13c43d1f32302a5162268f057b62a957717a311b
craftpine <hoangdai9895@gmail.com>
2026-02-22 01:29:49 +0700

    ref(): Refactor code structure for improved readability and maintainability

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,34 +1,17 @@
 import Versions from './components/Versions'
-import electronLogo from './assets/electron.svg'
 
 function App(): React.JSX.Element {
-  const ipcHandle = (): void => window.electron.ipcRenderer.send('ping')
 
   return (
-    <>
-      <img alt="logo" className="logo" src={electronLogo} />
-      <div className="creator">Powered by electron-vite</div>
-      <div className="text">
-        Build an Electron app with <span className="react">React</span>
-        &nbsp;and <span className="ts">TypeScript</span>
+    <div className="min-h-screen bg-white text-white">
+      <div className="flex flex-col items-center justify-center min-h-screen gap-6">
+        <h1 className='text-blue-500 text-4xl font-bold'>
+
+          123123
+        </h1>
+        <Versions></Versions>
       </div>
-      <p className="tip">
-        Please try pressing <code>F12</code> to open the devTool
-      </p>
-      <div className="actions">
-        <div className="action">
-          <a href="https://electron-vite.org/" target="_blank" rel="noreferrer">
-            Documentation
-          </a>
-        </div>
-        <div className="action">
-          <a target="_blank" rel="noreferrer" onClick={ipcHandle}>
-            Send IPC
-          </a>
-        </div>
-      </div>
-      <Versions></Versions>
-    </>
+    </div>
   )
 }
 
