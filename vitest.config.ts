4783517d6e68c54f8de02295e692dfb63a25bd04
craftpine <hoangdai9895@gmail.com>
2026-03-11 10:34:18 +0700

    feat(): setup unit test

---
+++ vitest.config.ts
@@ -0,0 +1,17 @@
+import { resolve } from 'path'
+import { defineConfig } from 'vitest/config'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  test: {
+    environment: 'jsdom',
+    globals: true,
+    setupFiles: ['./src/test/setup.ts'],
+  },
+  resolve: {
+    alias: {
+      '@renderer': resolve(__dirname, 'src/renderer/src'),
+    },
+  },
+})
